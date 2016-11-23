f1d0c870a420c409228559afd73ca575446115a5
Jenny Berger <jenny.a.berger@gmail.com>
2016-11-23 14:25:31 +0100

    Let TopicActivity recognize what main button was clicked

--- app/src/main/java/com/easylearning/easylearning/MainActivity.java
+++ app/src/main/java/com/easylearning/easylearning/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,26 +16,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessageElectrostaticField(View view) {
         Intent intent = new Intent(this, TopicActivity.class);
+        intent.putExtra("topic", "electrostaticField");
         startActivity(intent);
     }
 
     public void sendMessageElectricalFlowField(View view) {
         Intent intent = new Intent(this, TopicActivity.class);
+        intent.putExtra("topic", "electricalFlowField");
         startActivity(intent);
     }
 
     public void sendMessageElectricalNetwork(View view) {
         Intent intent = new Intent(this, TopicActivity.class);
+        intent.putExtra("topic", "electricalNetwork");
         startActivity(intent);
     }
 
     public void sendMessageStationaryMagneticField(View view) {
         Intent intent = new Intent(this, TopicActivity.class);
+        intent.putExtra("topic", "stationaryMagneticField");
         startActivity(intent);
     }
 
     public void sendMessageElectromagneticField(View view) {
         Intent intent = new Intent(this, TopicActivity.class);
+        intent.putExtra("topic", "electromagneticField");
         startActivity(intent);
     }
 }
--- app/src/main/java/com/easylearning/easylearning/TopicActivity.java
+++ app/src/main/java/com/easylearning/easylearning/TopicActivity.java
@@ -2,6 +2,7 @@ package com.easylearning.easylearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TopicActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class TopicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic);
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+        String output = extras.getString("topic");
+        TextView tv = (TextView) findViewById(R.id.textview);
+        tv.setText(output);
     }
 }
